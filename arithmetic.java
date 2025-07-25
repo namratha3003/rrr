01afed0857469c2a815f567c0d13ba22e047c028
namratha3003 <245122749052@mvsrec.edu.in>
2025-07-25 11:58:32 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,14 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 2;
+        int sum = x + y;
+        System.out.println("x + y = " + sum);
+        int difference = x - y;
+        System.out.println("x - y = " + difference);
+        int product = x * y;
+        System.out.println("x * y = " + product);
+    }
+}
+
+
